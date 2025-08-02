33ee07a6fb11c8b74e32ca405a69a2732c77765f
Sam-Delrio <vinter.delrio@proton.me>
2025-08-01 19:44:37 -0500

    6. Se agrega popup de ingreso

--- landing/assets/js/scripts.js
+++ landing/assets/js/scripts.js
@@ -1,1 +1,26 @@
-alert("Hola Mundo");
+//boton login
+document.addEventListener("DOMContentLoaded", function () {
+  const popup = document.getElementById("login-popup");
+  const ingresoBtn = document.querySelector(".ingreso");
+  const closeBtn = document.querySelector(".close-btn");
+
+  if (ingresoBtn) {
+    ingresoBtn.addEventListener("click", () => {
+      popup.style.display = "block";
+    });
+  }
+
+  if (closeBtn) {
+    closeBtn.addEventListener("click", () => {
+      popup.style.display = "none";
+    });
+  }
+  
+  // Cerrar si el usuario hace clic fuera del popup
+  window.addEventListener("click", function (e) {
+    if (e.target === popup) {
+      popup.style.display = "none";
+    }
+  });
+});
+
